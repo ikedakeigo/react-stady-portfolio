f70bf0ce8045cf53f75c12e5d0d5fd0404a74e97
silverskg <chokichoki_gm@i.softbank.jp>
2023-06-14 17:05:18 +0900

    react-bootstrap

---
+++ src/components/BlogPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BlogPage = () => {
+  return (
+    <div className="container">
+      <h1>ブログ</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptas reprehenderit, qui perferendis assumenda cumque quos non nesciunt laboriosam iure rerum porro iste facilis inventore et harum natus. Cupiditate rem magni odio neque. Delectus magni aspernatur similique perferendis tempore, minus eius eligendi mollitia rem? Voluptatibus ea eligendi cum quaerat consectetur asperiores commodi, natus aut dolores corrupti molestias debitis ullam id? Odio iste consequuntur numquam incidunt! Quaerat non at officiis sit facilis eligendi iste libero repellat aut ratione deserunt, ullam quae numquam sed facere quasi reiciendis, ab unde accusamus? Error incidunt quo voluptatem eveniet non illum earum quaerat voluptas quisquam placeat!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptas reprehenderit, qui perferendis assumenda cumque quos non nesciunt laboriosam iure rerum porro iste facilis inventore et harum natus. Cupiditate rem magni odio neque. Delectus magni aspernatur similique perferendis tempore, minus eius eligendi mollitia rem? Voluptatibus ea eligendi cum quaerat consectetur asperiores commodi, natus aut dolores corrupti molestias debitis ullam id? Odio iste consequuntur numquam incidunt! Quaerat non at officiis sit facilis eligendi iste libero repellat aut ratione deserunt, ullam quae numquam sed facere quasi reiciendis, ab unde accusamus? Error incidunt quo voluptatem eveniet non illum earum quaerat voluptas quisquam placeat!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptas reprehenderit, qui perferendis assumenda cumque quos non nesciunt laboriosam iure rerum porro iste facilis inventore et harum natus. Cupiditate rem magni odio neque. Delectus magni aspernatur similique perferendis tempore, minus eius eligendi mollitia rem? Voluptatibus ea eligendi cum quaerat consectetur asperiores commodi, natus aut dolores corrupti molestias debitis ullam id? Odio iste consequuntur numquam incidunt! Quaerat non at officiis sit facilis eligendi iste libero repellat aut ratione deserunt, ullam quae numquam sed facere quasi reiciendis, ab unde accusamus? Error incidunt quo voluptatem eveniet non illum earum quaerat voluptas quisquam placeat!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptas reprehenderit, qui perferendis assumenda cumque quos non nesciunt laboriosam iure rerum porro iste facilis inventore et harum natus. Cupiditate rem magni odio neque. Delectus magni aspernatur similique perferendis tempore, minus eius eligendi mollitia rem? Voluptatibus ea eligendi cum quaerat consectetur asperiores commodi, natus aut dolores corrupti molestias debitis ullam id? Odio iste consequuntur numquam incidunt! Quaerat non at officiis sit facilis eligendi iste libero repellat aut ratione deserunt, ullam quae numquam sed facere quasi reiciendis, ab unde accusamus? Error incidunt quo voluptatem eveniet non illum earum quaerat voluptas quisquam placeat!</p>
+    </div>
+  );
+};
+
+export default BlogPage;
